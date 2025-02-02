0c2846c8639f0e2ced54ad291de600550dc7081f
ErNeRooo <erneroooooo@gmail.com>
2025-02-02 18:44:53 +0100

    cool hover animation

--- src/app/components/ServiceButton/ServiceButton.tsx
+++ src/app/components/ServiceButton/ServiceButton.tsx
@@ -1,15 +1,19 @@
 import styles from "./ServiceButton.module.scss";
 
-const ServiceButton = ({ title }: Props) => {
+const ServiceButton = ({ title, japaneseTitle }: Props) => {
   return (
     <div className={styles.ServiceButton}>
-      <label>{title}</label>
+      <div className={styles.inner}>
+        <label>{japaneseTitle}</label>
+        <label>{title}</label>
+      </div>
     </div>
   );
 };
 
 interface Props {
   title: string;
+  japaneseTitle: string;
 }
 
 export default ServiceButton;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,42 @@
 import Link from "next/link";
 import ServiceButton from "./components/ServiceButton/ServiceButton";
 import styles from "./page.module.scss";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1 className={styles.englishTitle}>Welcome to Japanese Grind</h1>
-      <h1 className={styles.japaneseTitle}>{"日本のグラインドへようこそ"}</h1>
-      <h1 className={styles.japaneseTitlee}>日本のグラインドへようこそ</h1>
       <div className={styles.container}>
-        <Link href="/vocabulary" className={styles.link}>
-          <ServiceButton title="Vocabulary Learning" />
-        </Link>
-        <Link href="/kana" className={styles.link}>
-          <ServiceButton title="Kana Learning" />
-        </Link>
-        <Link href="/dictionary" className={styles.link}>
-          <ServiceButton title="Dictionary" />
-        </Link>
+        <div className={styles.verticalContainer}>
+          <div className={styles.header}>
+            <label className={styles.englishTitle}>
+              {"Welcome to Japanese Grind"}
+            </label>
+            <label className={styles.japaneseTitle}>
+              {" 日本語のグラインドへようこそ"}
+            </label>
+          </div>
+
+          <div className={styles.buttons}>
+            <Link href="/vocabulary" className={styles.link}>
+              <ServiceButton title="Vocabulary" japaneseTitle="語彙" />
+            </Link>
+            <Link href="/kana" className={styles.link}>
+              <ServiceButton title="Hiragana" japaneseTitle="ひらがな" />
+            </Link>
+            <Link href="/kana" className={styles.link}>
+              <ServiceButton title="Katakana" japaneseTitle="かたかな" />
+            </Link>
+            <Link href="/dictionary" className={styles.link}>
+              <ServiceButton title="Dictionary" japaneseTitle="辞書" />
+            </Link>
+          </div>
+        </div>
+        <div className={styles.verticalContainer}>
+          <Image src="/logo.svg" alt="logo" width={400} height={400} />
+        </div>
       </div>
+      <div className={styles.container}></div>
     </div>
   );
 }
